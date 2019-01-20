90f116949b968c731ff1c52c8175fd0387485c2f
herojeff02 <herojeff02@gmail.com>
2019-01-20 17:29:02 +0900

    Added 3 fragments, linked fragments to bottom navigation bar

--- app/src/main/java/com/herojeff/sulchedule/MainActivity.java
+++ app/src/main/java/com/herojeff/sulchedule/MainActivity.java
@@ -3,38 +3,57 @@ package com.herojeff.sulchedule;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_today:
-                    return true;
-                case R.id.navigation_past:
-                    return true;
-                case R.id.navigation_traffic:
-                    return true;
-            }
-            return false;
-        }
-    };
+    FrameLayout frameLayout;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        frameLayout = findViewById(R.id.fragment_container);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragment_container, new TodayFragment());
+        transaction.commit();
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_today:
+                        fragment = TodayFragment.newInstance("test1", "test2");
+                        fragment = new TodayFragment();
+                        break;
+                    case R.id.navigation_past:
+//                        fragment = PastFragment.newInstance("test1", "test2");
+                        fragment = new PastFragment();
+//                        difference?
+                        break;
+                    case R.id.navigation_traffic:
+                        fragment = TrafficFragment.newInstance("test1", "test2");
+//                        fragment = new TrafficFragment();
+//                        difference?
+                        break;
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.commit();
+                return false;
+            }
+        });
     }
 
 }
